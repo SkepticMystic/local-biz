d771778fcecb9928a557f4524a769a30e2927da8
SkepticMystic <rossk29@gmail.com>
2025-12-30 13:04:05 +0200

    markdown line breaks

--- src/lib/utils/markdown/markdown.util.ts
+++ src/lib/utils/markdown/markdown.util.ts
@@ -5,5 +5,13 @@ import type { IHTML } from "../html/html.util";
 export const Markdown = {
   strip,
   to_html: (md: string) =>
-    marked.parse(md, { async: false }) as IHTML.Prerendered,
+    marked.parse(md, {
+      gfm: true,
+      async: false,
+
+      // NOTE: If true, add <br> on a single line break
+      // (copies GitHub behavior on comments, but not on rendered markdown files).
+      // Requires gfm be true.
+      breaks: true,
+    }) as IHTML.Prerendered,
 };
